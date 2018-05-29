bbddd4b8515bb2a3474467d3b398ffcf92301343
Thomas Leitgeb <thomas.leitgeb@gmail.com>
2018-05-29 20:39:16 +0200

    Added exports for server adapters.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export * from './message-types';
 export * from './protocol';
 export * from './client-adapters/nativeClientAdapter';
 export * from './client-adapters/socketIOClientAdapter';
+export * from './server-adapters/nativeServerAdapter';
+export * from './server-adapters/socketIOServerAdapter';
